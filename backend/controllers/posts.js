48c50b9d6650b58f50f9915ce330fdbdace7ce2c
AD-code <cryptokalm82@gmail.com>
2022-08-05 03:41:59 +0200

    suppression post ok

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -3,25 +3,24 @@
 // importer le model post
 const Post = require("../models/Post");
 
-
-// importer fs pour gérer les images 
+// importer fs pour gérer les images
 const fs = require("fs");
 
-// Voir une post 
+// Voir une post
 exports.getOnePost = (req, res, next) => {
   Post.findOne({ _id: req.params.id })
     .then((post) => res.status(200).json(post))
     .catch((error) => res.status(404).json({ error }));
 };
 
-// Voir tous les posts 
+// Voir tous les posts
 exports.getAllPost = (req, res, next) => {
   Post.find()
     .then((posts) => res.status(200).json(posts))
     .catch((error) => res.status(400).json({ error }));
 };
 
-// Ajouter un post 
+// Ajouter un post
 exports.createPost = (req, res, next) => {
   const postObject = req.body.post;
   const post = new Post({
@@ -32,14 +31,14 @@ exports.createPost = (req, res, next) => {
   post
     .save()
     .then(() => {
-      res.status(201).json({ message: "Post créé !"});
+      res.status(201).json({ message: "Post créé !" });
     })
     .catch((error) => {
       res.status(400).json({ error });
     });
 };
 
-// Modifier un post 
+// Modifier un post
 exports.modifyPost = (req, res, next) => {
   const postObject = req.file
     ? {
@@ -69,22 +68,21 @@ exports.modifyPost = (req, res, next) => {
     });
 };
 
-
-// Supprimer un post 
+// Supprimer un post
 exports.deletePost = (req, res, next) => {
   Post.findOne({ _id: req.params.id })
     .then((post) => {
-      if (post.userId != req.auth.userId) {
+      if (post.userId != req.body.userId) {
         res.status(401).json({ message: "Non autorisé !" });
       } else {
-        const filename = post.imageUrl.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => {
-          Post.deleteOne({ _id: req.params.id })
-            .then(() => {
-              res.status(200).json({ message: "Objet supprimé !" })
-            })
-            .catch((error) => res.status(401).json({ error }));
-        });
+        // const filename = post.imageUrl.split("/images/")[1];
+        // fs.unlink(`images/${filename}`, () => {
+        Post.deleteOne({ _id: req.params.id })
+          .then(() => {
+            res.status(200).json({ message: "Post supprimé !" });
+          })
+          .catch((error) => res.status(401).json({ error }));
+        // });
       }
     })
     .catch((error) => res.status(400).json({ error }));
