c92b95df696c3d242037e98c057d1ce29454446b
AD-code <cryptokalm82@gmail.com>
2022-08-09 01:55:12 +0200

    delete fiche user + multer ok

--- backend/controllers/ficheUser.js
+++ backend/controllers/ficheUser.js
@@ -1,6 +1,8 @@
 // const jwt = require("jsonwebtoken")
 const FicheUser = require("../models/FicheUser");
 
+const fs = require("fs");
+
 exports.createFicheUser = (req, res, next) => {
   const ficheUserObject = req.body;
   delete ficheUserObject._id;
@@ -11,7 +13,7 @@ exports.createFicheUser = (req, res, next) => {
     photoProfilUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
-  })
+  });
   ficheUser
     .save()
     .then(() => res.status(201).json({ message: "Profil créée" }))
@@ -31,58 +33,56 @@ exports.getOneFicheUser = (req, res, next) => {
 };
 
 exports.modifyFicheUser = (req, res, next) => {
-  const ficheUserObject = req.image ? {
-      ...JSON.parse(req.body),
-      photoProfilUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  } : { ...req.body };
+  const ficheUserObject = req.image
+    ? {
+        ...JSON.parse(req.body),
+        photoProfilUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
 
   delete ficheUserObject.userId;
-  FicheUser.findOne({_id: req.params.id})
-      .then((ficheUser) => {
-          if (ficheUser.userId != req.auth.userId) {
-              res.status(401).json({ message : 'Non authorisé'});
-          } else {
-              FicheUser.updateOne({ _id: req.params.id}, { ...ficheUserObject, _id: req.params.id})
-              .then(() => res.status(200).json({message : 'Profil modifié!'}))
-              .catch(error => res.status(401).json({ error }));
-          }
-      })
-      .catch((error) => {
-          res.status(400).json({ error });
-      });
+  FicheUser.findOne({ _id: req.params.id })
+    .then((ficheUser) => {
+      if (ficheUser.userId != req.auth.userId) {
+        res.status(401).json({ message: "Non authorisé" });
+      } else {
+        FicheUser.updateOne(
+          { _id: req.params.id },
+          { ...ficheUserObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Profil modifié!" }))
+          .catch((error) => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 };
-
-// exports.modifyFicheUser = (req, res, next) => {
-//   FicheUser.findOne({ _id: req.params.id })
-//     .then((ficheUser) => {
-//       if (ficheUser.userId != req.body.userId) {
-//         res.status(400).json({ message: "Non autorisé !" });
-//       } else {
-//         FicheUser.updateOne({ _id: req.params.id }, {...req.body})
-//           .then(() => res.status(201).json({ message: "Profil modifié!" }))
-//           .catch((error) => res.status(400).json({ error }));
-//       }
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ error });
-//     });
-// };
 
 exports.deleteFicheUser = (req, res, next) => {
   FicheUser.findOne({ _id: req.params.id })
     .then((ficheUser) => {
-      if (ficheUser.userId != req.body.userId) {
-        res.status(401).json({ message: "Non autorisé !" });
+      if (ficheUser.userId != req.auth.userId) {
+        res.status(401).json({ message: "Non authorisé" });
       } else {
-        FicheUser.deleteOne({ _id: req.params.id })
-          .then(() => {
-            res.status(200).json({ message: "Profil supprimé !" });
-          })
-          .catch((error) => res.status(401).json({ error }));
+        const filename = ficheUser.photoProfilUrl.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+          FicheUser.deleteOne({ _id: req.params.id })
+            .then(() => {
+              res.status(200).json({ message: "Objet supprimé !" });
+            })
+            .catch((error) => res.status(401).json({ error }));
+        });
       }
     })
-    .catch((error) => res.status(400).json({ error }));
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
 };
+
+
 
 // exports.logout = (req, res, next) => {
 //   res.status(200).json({
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -16,11 +16,11 @@ router.post("/login", authCtrl.login);
 
 
 //FicheUser
-router.post("/ficheUser", auth, multer, ficheUserCtrl.createFicheUser);
-router.get("/ficheUser", auth, ficheUserCtrl.getAllFicheUser);
-router.get("/ficheUser/:id", auth, ficheUserCtrl.getOneFicheUser);
-router.put("/ficheUser/:id", auth, multer, ficheUserCtrl.modifyFicheUser);
-router.delete("/ficheUser/:id", auth, multer, ficheUserCtrl.deleteFicheUser);
+router.post("/fiche_user", auth, multer, ficheUserCtrl.createFicheUser);
+router.get("/fiche_user", auth, ficheUserCtrl.getAllFicheUser);
+router.get("/fiche_user/:id", auth, ficheUserCtrl.getOneFicheUser);
+router.put("/fiche_user/:id", auth, multer, ficheUserCtrl.modifyFicheUser);
+router.delete("/fiche_user/:id", auth, multer, ficheUserCtrl.deleteFicheUser);
 
 
 
